64a8d81b763fd8bf83ba428626cf9fe2399a8429
zaincashu <zaincashu@gmail.com>
2019-05-16 23:34:37 +0300

    updated #1

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var request = require('request');
 
 var app = express();
+var imageList = [];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+const FileCookieStore = require('tough-cookie-filestore2');
+const fs = require("fs");
+const Instagram = require('instagram-web-api');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,34 +43,133 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const fs = require("fs");
-const Instagram = require('instagram-web-api');
-async function login() {
-    var client = new Instagram({ username:'eyjnp5', password:'Samer@88' });
-    await client.login()
-    var user = await client.getUserByUsername({ username: 'samawisamer' });
-    console.log(user.id);
-    var insx = fs.readFileSync('./insx2.txt', 'UTF-8').split('\n');
-    var t1 = new Date().getTime();
-    for (var i=0; i<insx.length; i++) {
-        await follow(insx[i], 'Samer@88', user.id);
-        console.log('followed '+i);
-    }
-    var t2 = new Date().getTime();
-    var t = t2 - t1;
-    console.log('comple time '+(t/1000) + 'sec');
-}
-
-function follow(user, pass, id) {
-   return new Promise(async resolve => {
-       var client = new Instagram({ username:user, password:pass });
-    await client.login();
-    await client.follow({ userId: id });
-       resolve();
-   });
+async function main() {
     
+   await downloadP();
+    console.log('proccess run');
+    var client = await login('eyjnp2', "Samer@88");
+    var insx = fs.readFileSync('./insx.txt', 'UTF-8').split('\n');
+    for (var i=0; i<insx.length; i++) {
+        
+            var client = await login(insx[i], "Samer@88");
+            if (client === null) continue;
+       
+         var p =  await proc(client, imageList[i]);
+        if (p === null) {
+            fs.readFile('./block.txt', 'utf8', function(err, data) {
+                if (err) throw err;
+                if (data != '') data += '\n';
+                data += insx[i];
+                fs.writeFile('./block.txt', data);
+            });
+        console.log('error 1');
+        }
+        console.log('comple '+i); 
+        
+       await sleep(10);
+    }
+    console.log('comple full');
 }
 
-login();
+function login(user, pass) {
+    return new Promise(async resolve => {
+        
+            console.log('login ...');
+         if (user.indexOf('@') > -1)
+         user = user.match(/(.*?)@.*/)[1];
+        var cookie = new FileCookieStore('./cookies/'+user+'.json');
+        if (pass === undefined) 
+            var client = new Instagram({});
+        else
+            var client = new Instagram({ username:user, password:pass, cookieStore:cookie });
+        try {
+            await client.login();
+            resolve(client);
+            console.log('logined');
+        }
+        catch(e) {
+            resolve(null);
+            console.log("login fuiled");
+        }
+        
+        
+    });
+}
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+    resolve();
+    }, ms);
+  });
+}
+
+function proc(usr, name) {
+    return new Promise(async resolve => {
+        try {
+            var photo = './photos/'+name;
+            await usr.changeProfilePhoto({ photo });
+            resolve(true);
+        }
+        catch(e) {
+            resolve(null);
+            console.log(e);
+        }
+        
+    });
+}
+
+function downloadP() {
+    return new Promise(async resolve => {
+            var list1 = await search('صور انيقة');
+            var list2 = await search('صور انستغرام');
+            var list3 = await search('صور شخصية راقية');
+            var list = list1.concat(list2).concat(list3);
+            delete list1,list2,list3;
+        var download = function(uri, filename){
+            return new Promise(resolvee => {
+                 request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    if (res.headers['content-length'] === undefined)
+        {
+            resolvee();
+            return;
+        }
+        
+        imageList.push(filename);
+                 
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', resolvee);
+  });
+            });
+};
+        
+        console.log('downloading Images ...');
+        for (var ii=0; ii<list.length; ii++)
+            {
+            
+            console.log(ii);
+            await download(list[ii].url, './photos/ph_'+ii+'.jpg');
+            console.log('ccc');
+            }
+        console.log('dowloand comple');
+        resolve();
+
+     });
+}
+
+function search(text) {
+    return new Promise(resolve => {
+        var gis = require('g-i-s');
+        gis(text, (err, res) => {
+            if (err) throw err;
+            console.log(res.length);
+            resolve(res);
+        });
+    });
+}
+
+main();
 
 module.exports = app;
